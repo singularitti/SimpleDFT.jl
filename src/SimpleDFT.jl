bde1279b81bb88f5f1a392cde94f78aa9c2789a0
Wanja Schulze <wangenau@protonmail.com>
2022-09-15 10:37:32 +0200

    Build as a module

--- src/SimpleDFT.jl
+++ src/SimpleDFT.jl
@@ -1,10 +1,14 @@
+module SimpleDFT
+
 using Base.Math: libm
 using LinearAlgebra: det, Diagonal, dot, eigen, I, inv, tr
 
 using FFTW: fft, ifft
 
 include("atoms.jl")
+export Atoms
 include("scf.jl")
+export SCF, runSCF
 
 include("dft.jl")
 include("energies.jl")
@@ -13,3 +17,5 @@ include("operators.jl")
 include("potentials.jl")
 include("utils.jl")
 include("xc.jl")
+
+end
