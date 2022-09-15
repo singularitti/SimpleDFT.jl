840c8622079b7577a2b1705ea0ad9d539495f35d
Wanja Schulze <wangenau@protonmail.com>
2022-09-15 10:36:32 +0200

    Add timings to examples

--- examples.jl
+++ examples.jl
@@ -4,7 +4,8 @@ References for the DFT++ formulation: https://doi.org/10.1016/s0010-4655(00)0007
 Code annotations reference this master thesis: http://dx.doi.org/10.13140/RG.2.2.27958.42568/2
 More documentation can be found inside the eminus routines: https://gitlab.com/esp42/sage/eminus
 =#
-include("src/simpledft.jl")
+include("src/SimpleDFT.jl")
+using .SimpleDFT
 
 
 function calculate(atoms)
@@ -14,14 +15,14 @@ end
 
 
 H_atom = Atoms(["H"], [0.0 0.0 0.0;], 16.0, 16.0, [1.0], [60, 60, 60], [1.0])
-calculate(H_atom)
+@time calculate(H_atom)
 # Output:  Etot(["H"]) = -0.438413 Eh
 
 He_atom = Atoms(["He"], [0.0 0.0 0.0;], 16.0, 16.0, [2.0], [60, 60, 60], [2.0])
-calculate(He_atom)
+@time calculate(He_atom)
 # Output:  Etot(["He"]) = -2.632034 Eh
 
 # Experimental geometry from CCCBDB: https://cccbdb.nist.gov/exp2x.asp?casno=1333740&charge=0
 H2_atom = Atoms(["H", "H"], [0.0 0.0 0.0; 1.4 0.0 0.0], 16.0, 16.0, [1.0 1.0], [60, 60, 60], [2.0])
-calculate(H2_atom)
+@time calculate(H2_atom)
 # Output:  Etot(["H", "H"]) = -1.113968 Eh
