43871531b959c4ca56fe69a91054d224ebc88fa4
Wanja Schulze <wangenau@protonmail.com>
2022-09-19 11:23:11 +0200

    utils: Follow MINSTD in pseudo_uniform

--- src/utils.jl
+++ src/utils.jl
@@ -1,7 +1,7 @@
-"Pseudo uniform random number generator."
+"Lehmer random number generator, following MINSTD."
 function pseudo_uniform(size::Tuple{Int64,Int64}; seed::Int64 = 1234)
     U = zeros(ComplexF64, size)
-    mult = 16807
+    mult = 48271
     mod = (2^31) - 1
     x = (seed * mult + 1) % mod
     for i = 1:size[1]
